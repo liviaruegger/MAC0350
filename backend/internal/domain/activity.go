88b71abadd1813e0633a8a882fdf5674c7bf9116
Ana Lívia Rüegger Saldanha <analiviaruegger@gmail.com>
2025-05-11 19:55:01 -0300

    Update Activity entity in domain and add unit tests
    - add "Notes" field to entity
    - add pace logic to Activity
    - add unit tests for pace logic
    
    Signed-off-by: Ana Livia Ruegger Saldanha <ana.saldanha@usp.br>

--- backend/internal/domain/activity.go
+++ backend/internal/domain/activity.go
@@ -1,6 +1,7 @@
 package domain
 
 import (
+	"fmt"
 	"time"
 )
 
@@ -24,4 +25,28 @@ type Activity struct {
 	PoolSize     float64       `json:"pool_size"`     // Pool length in meters (0 if open water)
 	LocationType LocationType  `json:"location_type"` // "pool" or "open_water"
 	Intervals    []Interval    `json:"intervals"`     // Breakdown of the swim into segments
+	Notes        string        `json:"notes"`         // Optional notes
+}
+
+// AvgPacePer100m returns the average pace in seconds per 100 meters
+func (a Activity) AvgPacePer100m() float64 {
+	if a.Distance == 0 {
+		return 0
+	}
+
+	return a.Duration.Seconds() / a.Distance * 100
+}
+
+// AvgPaceFormatted returns the pace as a string in the format mm:ss per 100m;
+// If distance is 0, it returns "N/A"
+func (a Activity) AvgPaceFormatted() string {
+	pace := a.AvgPacePer100m()
+	if pace == 0 {
+		return "N/A"
+	}
+
+	minutes := int(pace) / 60
+	seconds := int(pace) % 60
+
+	return fmt.Sprintf("%02d:%02d", minutes, seconds)
 }
